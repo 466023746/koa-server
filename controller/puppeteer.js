a5f23ab2d7825f9046d80cdfb15d582ffbff64a2
娄涛 <loutao@corp.netease.com>
2018-12-17 14:27:52 +0800

    feat: puppteer

---
+++ controller/puppeteer.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.kaola.com');
+    await page.screenshot({path: 'example.png'});
+
+    await browser.close();
+})();
+
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://www.kaola.com', {waitUntil: 'networkidle2'});
+//     await page.pdf({path: 'hn.pdf', format: 'A4'});
+//
+//     await browser.close();
+// })();
+
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://www.kaola.com');
+//
+//     // Get the "viewport" of the page, as reported by the page.
+//     const dimensions = await page.evaluate(() => {
+//         return {
+//             width: document.documentElement.clientWidth,
+//             height: document.documentElement.clientHeight,
+//             deviceScaleFactor: window.devicePixelRatio
+//         };
+//     });
+//
+//     console.log('Dimensions:', dimensions);
+//
+//     await browser.close();
+// })();
